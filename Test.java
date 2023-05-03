9094a66df6b263fcc7dd4eec510c0735859b44d4
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2023-05-03 23:07:36 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,7 +9,7 @@ public class Test {
 	 A a = new B();      //upcasting
 	 B b = (B) new A();  //downcasting
 	 
-	 Object o = new A();
+Object o = new A();
 // object class reference can hold data of any type	 
 	 Object d=101; //int type
 	 int w=(int) d; //downcasting

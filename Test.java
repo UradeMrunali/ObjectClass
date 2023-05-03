f3c7fb90eab111efcb183eb536bc09d66b83377c
Mrunali <mrunaliurade26@gmail.com>
2023-05-03 23:06:39 +0530

    example of object class

---
+++ Test.java
@@ -0,0 +1,27 @@
+class A
+{
+	
+}
+class B extends A
+{
+	
+}
+
+
+public class Test {
+ public static void main(String[] args) {
+	 A a = new B();      //upcasting
+	 B b = (B) new A();  //downcasting
+	 
+	 Object o = new A();
+	 
+	 
+
+// object class reference can hold data of any type	 
+	 Object d=101; //int type
+	 int w=(int) d; //downcasting
+	 
+	 d="hii";      //String type
+	 d=20.47;      //float type 
+ }
+}
